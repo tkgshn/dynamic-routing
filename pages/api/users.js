c36e841fc054067dccfc1b791caa34e36bbed306
Shunsuke Takagi <takagishunsuke1129@gmail.com>
2022-10-26 21:21:23 +0000

    キリのいいところまでは出来たが、この先詳細表示のところで詰まってる

---
+++ pages/api/users.js
@@ -0,0 +1,6 @@
+//適当に引っ張ってきたAPI
+export default async function handler(req, res) {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/')
+    const users = await response.json()
+    res.status(200).json({ users })
+}
